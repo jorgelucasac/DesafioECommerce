73977bcb641fa71b6c853b407a4708e3593ccbe8
jorgelucas <jorgelucasl91@gmail.com>
2021-10-06 19:53:07 -0300

    [FEATURE] Estrutura de notificações

---
+++ src/Desafio.ECommerce.Business/Interfaces/INotificador.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Desafio.ECommerce.Business.Notificacoes;
+
+namespace Desafio.ECommerce.Business.Interfaces
+{
+    public interface INotificador
+    {
+        bool TemNotificacoes();
+
+        IList<Notificacao> ObterNotificacoes();
+        void Notificar(Notificacao notificacao);
+        void Notificar(string chave, string mensagem);
+    }
+}
---
+++ src/Desafio.ECommerce.Business/Notificacoes/Notificador.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Desafio.ECommerce.Business.Interfaces;
+
+namespace Desafio.ECommerce.Business.Notificacoes
+{
+    public class Notificador : INotificador
+    {
+        private readonly IList<Notificacao> _notificacoes;
+
+        public Notificador()
+        {
+            _notificacoes = new List<Notificacao>();
+        }
+
+        public bool TemNotificacoes()
+        {
+            return _notificacoes.Any();
+        }
+
+        public IList<Notificacao> ObterNotificacoes()
+        {
+            return _notificacoes;
+        }
+
+        public void Notificar(Notificacao notificacao)
+        {
+            _notificacoes.Add(notificacao);
+        }
+
+        public void Notificar(string chave, string mensagem)
+        {
+            _notificacoes.Add(new Notificacao(chave, mensagem));
+        }
+    }
+}
