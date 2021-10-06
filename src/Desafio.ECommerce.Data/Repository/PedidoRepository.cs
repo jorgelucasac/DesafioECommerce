801a47d3b116b3dd3d29b48b9e9a840501c0f1b4
jorgelucas <jorgelucasl91@gmail.com>
2021-10-06 19:55:22 -0300

    [FEATURE] Implementação dos repositórios

---
+++ src/Desafio.ECommerce.Data/Repository/PedidoRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Desafio.ECommerce.Business.Interfaces;
+using Desafio.ECommerce.Business.Models;
+using Desafio.ECommerce.Business.Paginacao;
+using Desafio.ECommerce.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Desafio.ECommerce.Data.Repository
+{
+    public class PedidoRepository : Repository<Pedido>, IPedidoRepository
+    {
+        public PedidoRepository(AppDbContext context) : base(context) { }
+
+        public async Task<PagedResult<Pedido>> ObterPedidos(int pagina, int quantidade)
+        {
+            var qtdRegistrosPular = quantidade * (pagina - 1);
+            var query =  await Context.Pedidos
+                .Include(e => e.Equipe)
+                .Include(p => p.Produtos)
+                .Include(e => e.Endereco)
+                .Skip(qtdRegistrosPular).Take(quantidade).ToListAsync();
+
+            return new PagedResult<Pedido>()
+            {
+                List = query,
+                TotalResults = query.Count,
+                PageIndex = pagina,
+                PageSize = quantidade,
+            };
+        }
+    }
+}
---
+++ src/Desafio.ECommerce.Data/Repository/Repository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Desafio.ECommerce.Business.Interfaces;
+using Desafio.ECommerce.Business.Models;
+using Desafio.ECommerce.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Desafio.ECommerce.Data.Repository
+{
+    public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity, new()
+    {
+        protected readonly AppDbContext Context;
+        protected readonly DbSet<TEntity> DbSet;
+
+        protected Repository(AppDbContext context)
+        {
+            Context = context;
+            DbSet = Context.Set<TEntity>();
+        }
+
+        public virtual async Task<TEntity> ObterPorId(Guid id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public virtual async Task<List<TEntity>> ObterTodos()
+        {
+            return await DbSet.ToListAsync();
+        }
+
+        public void Dispose()
+        {
+            Context?.Dispose();
+        }
+    }
+}
