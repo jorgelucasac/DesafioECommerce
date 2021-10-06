6688731800d7e96f85af2b2bfc62319b3e95a878
jorgelucas <jorgelucasl91@gmail.com>
2021-10-06 19:52:23 -0300

    [FEATURE] Interfaces de repositórios

---
+++ src/Desafio.ECommerce.Business/Interfaces/IPedidoRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Desafio.ECommerce.Business.Models;
+using Desafio.ECommerce.Business.Paginacao;
+
+namespace Desafio.ECommerce.Business.Interfaces
+{
+    public interface IPedidoRepository : IRepository<Pedido>
+    {
+        Task<PagedResult<Pedido>> ObterPedidos(int pagina, int quantidade);
+    }
+}
---
+++ src/Desafio.ECommerce.Business/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Desafio.ECommerce.Business.Models;
+
+namespace Desafio.ECommerce.Business.Interfaces
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity
+    {
+        Task<TEntity> ObterPorId(Guid id);
+        Task<List<TEntity>> ObterTodos();
+    }
+}
