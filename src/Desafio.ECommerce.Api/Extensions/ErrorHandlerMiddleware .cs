3eb9b7decb8e82aa0e4b5210b32ffa5b19dd0fe3
jorgelucas <jorgelucasl91@gmail.com>
2021-10-06 20:01:30 -0300

    [FEATURE] Middlewares para tratar erros e acesso do usuário

---
+++ src/Desafio.ECommerce.Api/Extensions/ErrorHandlerMiddleware .cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Desafio.ECommerce.Api.Extensions
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public ErrorHandlerMiddleware(RequestDelegate next, ILogger<ErrorHandlerMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception exception)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+                response.StatusCode = StatusCodes.Status500InternalServerError;
+                await response.WriteAsJsonAsync(ObterErrosResposta());
+            }
+        }
+
+        private ValidationProblemDetails ObterErrosResposta()
+        {
+            return new ValidationProblemDetails(new Dictionary<string, string[]>())
+            {
+                Title = "Ocorreu um erro na sua requisição. Por favor, tente novamente!"
+            };
+        }
+    }
+}
---
+++ src/Desafio.ECommerce.Api/Extensions/SecurityMiddleware.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Desafio.ECommerce.Api.Extensions
+{
+    public class SecurityMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private AppSettings _appSettings;
+
+        public SecurityMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context, IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+
+            if (PossuiAcesso(context))
+                await _next(context);
+            else
+                await AcessoNegado(context);
+        }
+
+        private bool PossuiAcesso(HttpContext context)
+        {
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+            return token == _appSettings.Token;
+        }
+
+        private async Task AcessoNegado(HttpContext context)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+            response.StatusCode = StatusCodes.Status401Unauthorized;
+            await response.WriteAsJsonAsync(new ValidationProblemDetails(new Dictionary<string, string[]>())
+            {
+                Title = "Não autorizado"
+            });
+        }
+
+    }
+}
